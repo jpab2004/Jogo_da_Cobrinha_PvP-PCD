e9f6cbbb9a70189a61c80aa14a42008aac09ba1e
jpab2004 <jparoucha@gmail.com>
2022-06-23 20:01:09 -0300

    Versão 1

--- Snake Game.py
+++ Snake Game.py
@@ -7,13 +7,35 @@ import sys
 
 pg.init()
 
-dis_width, dis_height = 500, 500
-row, col = 20, 20
-clock = 0.5
+flags = pg.SHOWN | pg.NOFRAME | pg.FULLSCREEN
+dis_width, dis_height = 1920, 1080
+col, row = int(dis_width / 100), int(dis_height / 100)
+square_w, square_h = dis_width / col, dis_height / row
+clock = 0.2
+
+IA = False
+
+font_scale = 1
+if row < col:
+    font_size = int(square_h * font_scale)
+else:
+    font_size = int(square_w * font_scale)
+
+dis = pg.display.set_mode((dis_width, dis_height), flags)
+pg.display.set_caption('Jogo da Cobrinha!')
+
+#font_style = pg.font.SysFont('Time New Roman', int(dis_width * font_scale))
+font_style = pg.font.SysFont('Comic Sans MS', font_size)
 
 game = np.zeros((row, col))
-head = [10, 10]
-game[head[0]][head[0]] = 2
+head = [0, 2]
+game[head[0]][head[1]] = 2
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+blue = (50, 50, 255)
+pink = (255, 50, 255)
 
 tail = []
 
@@ -28,8 +50,52 @@ for i in tail:
 
 dir = 1
 
-dis = pg.display.set_mode((dis_width, dis_height))
-pg.display.set_caption('Jogo da Cobrinha!')
+def find_cor(n):
+    if n == 0:
+        return white
+    elif n == 1:
+        return blue
+    elif n == 2:
+        return pink
+    elif n == -1:
+        return red
+
+def line(start, end, color):
+    pg.draw.line(dis, color, start, end, 2)
+    return
+
+def write_s(mesg, pos, cor):
+    msg = font_style.render(mesg, True, cor)
+    dis.blit(msg, pos)
+    return
+
+def matrix():
+    pg.draw.rect(dis, black, (0, 0, dis_width, dis_height))
+
+    for i in range(1, row):
+        start = (0, square_h * i)
+        end = (dis_width, square_h * i)
+        line(start, end, white)
+    
+    for i in range(1, col):
+        start = (square_w * i, 0)
+        end = (square_w * i, dis_height)
+        line(start, end, white)
+
+    return
+
+def update_m():
+    matrix()
+    for i in range(row):
+        for j in range(col):
+            msg = '%i' % game[i][j]
+            text_w, text_h = font_style.size(msg)
+
+            cor = find_cor(game[i][j])
+
+            pos = [(square_w * j) + (square_w / 2) - (text_w / 2), (square_h * i) + (square_h / 2) - (text_h / 2)]
+            write_s(msg, pos, cor)
+    pg.display.update()
 
 def game_over():
     print('Game Over!')
@@ -92,8 +158,10 @@ def update(dir):
     print(tb(game))
     return head_new
 
+
 create_apple()
 print(tb(game))
+update_m()
 t.sleep(clock)
 
 while True:
@@ -120,5 +188,9 @@ while True:
         print('Throwaway!')
 
     head = update(dir)
+    update_m()
+
+    if IA:
+        clock = 1 / (len(tail) + 1)
     
     t.sleep(clock)
